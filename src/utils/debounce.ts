1ee50297ce3357b0b4999d11d4ddf9f813c42d40
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-08-28 20:27:33 +0545

    Removed unnecessary files

--- src/utils/debounce.ts
+++
@@ -1,8 +0,0 @@
-export const debounce = (func: () => void, w: number) => {
-    let t: number;
-    return function () {
-      clearTimeout(t);
-      t = window.setTimeout(() => func(), w);
-    };
-  };
-  
