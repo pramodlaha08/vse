3497da8505be00eba3c80d11b5705b4a084000b7
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-08-28 20:24:42 +0545

    Removed lib directory

--- types/lib/scroll-reveal.d.ts
+++
@@ -1,8 +0,0 @@
-interface HTMLELEMETWithScrollReveal extends HTMLElement {
-    __scrollReveal__?: () => void;
-}
-declare const VueScroll: {
-    mounted: (el: HTMLELEMETWithScrollReveal, binding: any) => void;
-    unmounted: (el: HTMLELEMETWithScrollReveal) => void;
-};
-export default VueScroll;
