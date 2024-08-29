84025cad54ac3168eaf539e16c7a9dd18c2b7c5f
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-08-29 11:56:12 +0545

    Updated Code

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,26 @@ import Main from "./components/Main.vue";
   <div class="background"></div>
   <Main />
 </template>
-<style scoped lang="scss">
+<style scoped>
 .background-img,
 .background {
   margin: 0;
   width: 100vw;
   max-width: 100%;
   height: 100vh;
-  background-color: #824e0063;
   position: fixed;
   overflow: hidden;
   z-index: -1;
-
-  &.background-img {
-    background-image: url('../../assets/background.jpg');
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-  }
 }
 
+.background-img {
+  background-image: url('../../assets/background.jpg');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
 
+.background {
+  background-color: #824e0063;
+}
 </style>
